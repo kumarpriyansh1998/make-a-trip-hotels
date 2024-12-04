aae078f5415dc4d500c528705ae579a196e096a8
Kumar Priyansh <43110812+kumarpriyansh1998@users.noreply.github.com>
2024-12-04 15:34:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+const port = 3000;
 let cors = require('cors');
 app.use(cors());
 
